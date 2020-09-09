877cff63869f10a5a7fcc3d0e4d7d977459fb79c
0scarB <oscarb@protonmail.com>
2020-09-09 16:25:39 +0200

    Support nested tail calls

--- tail_recursive/__init__.py
+++ tail_recursive/__init__.py
@@ -1,14 +1,255 @@
-"""Simple package for your tail recursion needs."""
+"""Simple package for your tail recursion needs.
 
+Use the ``tail_recursive`` decorator to define tail_recursive functions.
+
+Example::
+
+    import sys
+    from tail_recursive import tail_recursive
+
+
+    @tail_recursive
+    def mul(a, b):
+        return a * b
+
+    @tail_recursive
+    def factorial(n):
+        if n == 1:
+            return n
+        # Nested tail calls are supported by default.
+        return mul.tail_call(n, factorial.tail_call(n - 1))
+
+    
+    # Calls to tail recursive functions will not exceed the maximum recursion
+    # depth, because functions are called sequentially.
+    factorial(sys.getrecursionlimit() + 1)
+"""
+
+import abc
 from dataclasses import dataclass
-from functools import wraps
-from itertools import chain
-from typing import Any, Callable, Dict, Tuple
+import enum
+import functools
+import itertools
+from typing import Any, Callable, Dict, Iterable, List, Optional, Type, Union
+
+
+@dataclass
+class _FuncStore:
+    func: Callable[..., Any]
+
+    @property
+    def _func_repr(self) -> str:
+        return repr(self.func)
+
+
+@dataclass
+class _ArgsAndKwargsStore:
+    args: List[Any]
+    kwargs: Dict[str, Any]
+
+    @property
+    def _args_and_kwargs_string(self) -> str:
+        return ', '.join(itertools.chain(
+            (repr(arg) for arg in self.args),
+            (f"{name}={repr(val)}" for name, val in self.kwargs.items())
+        ))
 
 
 @dataclass(init=False)
-class tail_recursive:
+class _IndexedArgsAndKwargsAccess:
+    length: int
+    _accessing_object: _ArgsAndKwargsStore
+    _last_arg_index: int
+    _kwargs_index_key_map: Dict[int, str]
+
+    def __init__(self, _accessing_object: _ArgsAndKwargsStore):
+        self._accessing_object = _accessing_object
+        self._last_arg_index = len(self._accessing_object.args) - 1
+        self.length = (
+            self._last_arg_index
+            + len(self._accessing_object.kwargs) + 1
+        )
+        self._kwargs_index_key_map = {
+            index: key for index, key in zip(
+                range(self._last_arg_index + 1, self.length),
+                self._accessing_object.kwargs.keys()
+            )
+        }
+
+    def get(self, index: int) -> Any:
+        if index > self._last_arg_index:
+            return self._accessing_object.kwargs[self._kwargs_index_key_map[index]]
+        return self._accessing_object.args[index]
+
+    def set(self, index: int, val: Any) -> None:
+        if index > self._last_arg_index:
+            self._accessing_object.kwargs[self._kwargs_index_key_map[index]] = val
+        else:
+            self._accessing_object.args[index] = val
+
+
+# Mypy doesn't currently allow abstract dataclasses (see https://github.com/python/mypy/issues/5374).
+@dataclass  # type: ignore
+class TailCall(abc.ABC, _FuncStore, _ArgsAndKwargsStore):
+    """Stores information necessary to lazily execute a function in the future."""
+
+    def __repr__(self) -> str:
+        return f"{tail_recursive(self.func)}.tail_call({self._args_and_kwargs_string})"
+
+    @ abc.abstractmethod
+    def resolve(self):
+        """Lazily and sequentially evaluates recursive tail calls while maintaining same size of callstack."""
+        ...
+
+
+class TailCallWithoutNestedCallResolution(TailCall):
+
+    def resolve(self):
+        resolution_value = self.func(*self.args, **self.kwargs)
+        while isinstance(resolution_value, TailCall):
+            resolution_value = self.func(
+                *resolution_value.args,
+                **resolution_value.kwargs
+            )
+        return resolution_value
+
+
+@dataclass(init=False)
+class _TailCallStackItem:
+    tail_call: TailCall
+    indexed_args_and_kwargs: _IndexedArgsAndKwargsAccess
+    resolving_arg_or_kwarg_with_index: Optional[int]
+
+    def __init__(self, tail_call: TailCall):
+        self.tail_call = tail_call
+        self.indexed_args_and_kwargs = _IndexedArgsAndKwargsAccess(tail_call)
+        self.resolving_arg_or_kwarg_with_index = None
+
+
+@dataclass(init=False)
+class _TailCallStack:
+    stack: List[_TailCallStackItem]
+    length: int
+
+    def __init__(self, initial_item: TailCall):
+        self.stack = [_TailCallStackItem(initial_item)]
+        self.length = 1
+
+    @property
+    def last_item(self):
+        return self.stack[-1]
+
+    def push(self, item: TailCall):
+        self.stack.append(_TailCallStackItem(item))
+        self.length += 1
+
+    def pop_item_resolution(self):
+        tail_call_with_fully_resolved_args_and_kwargs = self.stack.pop().tail_call
+        self.length -= 1
+        return tail_call_with_fully_resolved_args_and_kwargs.func(
+            *tail_call_with_fully_resolved_args_and_kwargs.args,
+            **tail_call_with_fully_resolved_args_and_kwargs.kwargs
+        )
+
+    def set_arg_or_kwarg_of_last_item_to_resolution(self, resolution: Any):
+        self.last_item.indexed_args_and_kwargs.set(
+            self.last_item.resolving_arg_or_kwarg_with_index,
+            resolution
+        )
+
+
+class TailCallWithNestedCallResolution(TailCall):
+
+    def __init__(self, func: Callable[..., Any], args: List[Any], kwargs: Dict[str, Any]):
+        # ``setattr`` stops mypy complaining.
+        # Seems to be related to this issue https://github.com/python/mypy/issues/2427.
+        setattr(self, "func", func)
+        self.args = args
+        self.kwargs = kwargs
+
+    def resolve(self):
+        tail_call_stack = _TailCallStack(initial_item=self)
+        while True:
+            if tail_call_stack.last_item.resolving_arg_or_kwarg_with_index is None:
+                start_arg_index = 0
+            else:
+                start_arg_index = tail_call_stack.last_item.resolving_arg_or_kwarg_with_index + 1
+            for arg_index in range(start_arg_index, tail_call_stack.last_item.indexed_args_and_kwargs.length):
+                arg = tail_call_stack.last_item.indexed_args_and_kwargs.get(
+                    arg_index
+                )
+                if isinstance(arg, TailCall):
+                    tail_call_stack.last_item.resolving_arg_or_kwarg_with_index = arg_index
+                    tail_call_stack.push(arg)
+                    break
+            # Else block is evaluated if loop is not broken out of.
+            else:
+                resolution = tail_call_stack.pop_item_resolution()
+                if isinstance(resolution, TailCall):
+                    tail_call_stack.push(resolution)
+                elif tail_call_stack.length > 0:
+                    tail_call_stack.set_arg_or_kwarg_of_last_item_to_resolution(
+                        resolution
+                    )
+                else:
+                    return resolution
+
+
+@enum.unique
+class NestedCallMode(enum.Enum):
+    """Different ways of resolving nested tail calls."""
+
+    DO_NOT_RESOLVE_NESTED_CALLS: str = "do_not_resolve_nested_calls"
+    RESOLVE_NESTED_CALLS: str = "resolve_nested_calls"
+
+
+NESTED_CALL_MODE_TAILCALL_SUBCLASS_MAP: Dict[NestedCallMode, Type[TailCall]] = {
+    NestedCallMode.DO_NOT_RESOLVE_NESTED_CALLS: TailCallWithoutNestedCallResolution,
+    NestedCallMode.RESOLVE_NESTED_CALLS: TailCallWithNestedCallResolution,
+}
+
+
+@dataclass(init=False)
+class TailCallable(_FuncStore):
+
+    nested_call_mode: NestedCallMode
+
+    def __init__(self, func: Callable[..., Any], *, nested_call_mode: Union[NestedCallMode, str] = NestedCallMode.RESOLVE_NESTED_CALLS):
+        functools.update_wrapper(self, func)
+        # ``setattr`` stops mypy complaining.
+        # Seems to be related to this issue https://github.com/python/mypy/issues/2427.
+        setattr(self, "func", func)
+        if isinstance(nested_call_mode, NestedCallMode):
+            self.nested_call_mode = nested_call_mode
+        else:
+            self.nested_call_mode = NestedCallMode(nested_call_mode)
+
+    def __repr__(self) -> str:
+        return f"{tail_recursive.__qualname__}(func={self._func_repr})"
+
+    def __call__(self, *args, **kwargs) -> Any:
+        return self.tail_call(*args, **kwargs).resolve()
+
+    def tail_call(self, *args, **kwargs) -> TailCall:
+        """Passes arguments to a tail recursive function so that it may lazily called.
+
+        This method should be called as the single return value of a function. This
+        enables the function to be called once the after its caller function has been
+        garbage collected.
+
+        Example::
+
+            def f():
+                return tail_recursive_function.tail_call(...)
+        """
+        return NESTED_CALL_MODE_TAILCALL_SUBCLASS_MAP[self.nested_call_mode](func=self.func, args=list(args), kwargs=kwargs)
+
+
+def tail_recursive(_func=None, *, nested_call_mode=NestedCallMode.RESOLVE_NESTED_CALLS):
     """A decorator that gives your functions the ability to be tail recursive.
+
+    Args:
+        nested_call_mode: Defines the way in which nested calls are resolved.
 
     Example::
 
@@ -39,63 +280,10 @@ class tail_recursive:
     Methods:
         tail_call(*args, **kwargs)
     """
+    def decorator(func):
+        return TailCallable(func, nested_call_mode=nested_call_mode)
 
-    func: Callable[..., Any]
-    args: Tuple[Any, ...]
-    kwargs: Dict[str, Any]
-    has_been_tail_called: bool
-
-    def __init__(self, func: Callable[..., Any]):
-        """Assigns the ``func`` attribute to the decorated function."""
-        self.func = func  # type: ignore
-        self.has_been_tail_called = False
-
-    def __repr__(self) -> str:
-        class_string: str = type(self).__qualname__
-        func_string: str = repr(self.func)
-        object_string: str = f"{class_string}(func={func_string})"
-        if self.has_been_tail_called:
-            args_string = ', '.join(chain(
-                (repr(arg) for arg in self.args),
-                (f"{name}={repr(val)}" for name, val in self.kwargs.items())
-            ))
-            return f"{object_string}.tail_call({args_string})"
-        return object_string
-
-    def __call__(self, *args, **kwargs) -> Any:
-        @wraps(self.func)
-        def wrapper(*args, **kwargs) -> Any:
-            # If ``return_value`` is an instance of ``tail_recursive`` then
-            # ``return_value`` will be reassigned to the return value of the
-            # function stored as ``func`` called with the arguments set in the
-            # call to tail_recursion.
-            return_value: tail_recursive = self.tail_call(*args, **kwargs)
-            while isinstance(
-                (return_value := return_value.func(
-                    *return_value.args,
-                    **return_value.kwargs
-                )),
-                type(self)
-            ):
-                pass
-            # Once ``return_value`` is no longer an instance of ``tail_recursive``, it
-            # is returned.
-            return return_value
-        return wrapper(*args, **kwargs)
-
-    def tail_call(self, *args, **kwargs) -> 'tail_recursive':
-        """Passes arguments to a tail recursive function so that it may lazily called.
-
-        This method should be called as the single return value of a function. This
-        enables the function to be called once the after its caller function has been
-        garbage collected.
-
-        Example::
-
-            def f():
-                return tail_recursive_function.tail_call(...)
-        """
-        self.args = args
-        self.kwargs = kwargs
-        self.has_been_tail_called = True
-        return self
+    if _func is None:
+        return decorator
+    else:
+        return decorator(_func)
--- tests.py
+++ tests.py
@@ -1,7 +1,17 @@
+import concurrent.futures
 import sys
+import time
+
 import pytest
 
-from tail_recursive import tail_recursive
+from tail_recursive import tail_recursive, NestedCallMode
+
+
+def non_recursive_factorial(n):
+    result = 1
+    for coefficient in range(2, n + 1):
+        result *= coefficient
+    return result
 
 
 def test__repr__():
@@ -27,86 +37,239 @@ def test__repr__():
     )) == f"tail_recursive(func=" + repr(func) + ").tail_call('first_arg', 2, [], first_kwarg='1', second_kwarg=2, third_kwarg={})"
 
 
+def test_nested_tail_call_mode_raises_exception_for_unknown_mode():
+
+    with pytest.raises(ValueError) as excinfo:
+        @tail_recursive(nested_call_mode="not_a_mode")
+        def _():
+            pass
+
+    assert "'not_a_mode' is not a valid NestedCallMode" in str(excinfo.value)
+
+
+def test_nested_tail_call_mode_converts_string_to_mode():
+
+    @tail_recursive(nested_call_mode="resolve_nested_calls")
+    def _():
+        pass
+
+    @tail_recursive(nested_call_mode="do_not_resolve_nested_calls")
+    def _():
+        pass
+
+
 def test_factorial_fails_when_max_recursion_depth_is_reached():
+    for nested_call_mode in (NestedCallMode.RESOLVE_NESTED_CALLS, NestedCallMode.DO_NOT_RESOLVE_NESTED_CALLS):
 
-    @tail_recursive
-    def factorial(n, accumulator=1):
-        if n == 1:
-            return accumulator
-        return factorial(n - 1, n * accumulator)
+        @tail_recursive(nested_call_mode=nested_call_mode)
+        def factorial(n, accumulator=1):
+            if n == 1:
+                return accumulator
+            return factorial(n - 1, n * accumulator)
 
-    assert factorial(1) == 1
-    assert factorial(3) == 6
-    assert factorial(4) == 24
-    assert factorial(6) == 720
+        assert factorial(1) == non_recursive_factorial(1) == 1
+        assert factorial(3) == non_recursive_factorial(3) == 6
+        assert factorial(4) == non_recursive_factorial(4) == 24
+        assert factorial(6) == non_recursive_factorial(6) == 720
 
-    n = sys.getrecursionlimit() + 1
-    with pytest.raises(RecursionError) as excinfo:
-        factorial(n)
-    assert "maximum recursion depth" in str(excinfo.value)
+        n = sys.getrecursionlimit() + 1
+        with pytest.raises(RecursionError) as excinfo:
+            factorial(n)
+        assert "maximum recursion depth" in str(excinfo.value)
 
 
 def test_factorial_succeeds_with_tail_recursion():
+    for nested_call_mode in (NestedCallMode.RESOLVE_NESTED_CALLS, NestedCallMode.DO_NOT_RESOLVE_NESTED_CALLS):
+
+        @tail_recursive(nested_call_mode=nested_call_mode)
+        def factorial(n, accumulator=1):
+            if n == 1:
+                return accumulator
+            return factorial.tail_call(n - 1, n * accumulator)
+
+        assert factorial(1) == non_recursive_factorial(1) == 1
+        assert factorial(3) == non_recursive_factorial(3) == 6
+        assert factorial(4) == non_recursive_factorial(4) == 24
+        assert factorial(6) == non_recursive_factorial(6) == 720
+
+        n = sys.getrecursionlimit() + 100
+        assert factorial(n) == non_recursive_factorial(n)
+
+
+def test_multithreaded_factorial_succeeds_with_tail_recursion():
+    """Test for thread safety."""
+    for nested_call_mode in (NestedCallMode.RESOLVE_NESTED_CALLS, NestedCallMode.DO_NOT_RESOLVE_NESTED_CALLS):
+
+        @tail_recursive(nested_call_mode=nested_call_mode)
+        def factorial(n, accumulator=1):
+            time.sleep(0.001)
+            if n == 1:
+                return accumulator
+            return factorial.tail_call(n - 1, n * accumulator)
+
+        # If there is shared data accross multiple threads then the following
+        # may invoke a race condition.
+        n1 = 100
+        n2 = 6
+        ns = (n2, n1)
+        n_expected_result_map = {n: non_recursive_factorial(n) for n in ns}
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            # Runs factorial concurrently on different threads for each n of ns.
+            n_factorial_future_map = {
+                n: executor.submit(factorial, n) for n in ns}
+            concurrent.futures.wait(n_factorial_future_map.values())
+            assert n_factorial_future_map[n1].result(
+            ) == n_expected_result_map[n1]
+            assert n_factorial_future_map[n2].result(
+            ) == n_expected_result_map[n2]
+
+
+def test_multi_function_factorial_fails_with_nested_tail_call_resolution_disabled():
 
     @tail_recursive
-    def factorial(n, accumulator=1):
-        if n == 1:
-            return accumulator
-        return factorial.tail_call(n - 1, n * accumulator)
+    def mul(a, b):
+        return a * b
 
-    assert factorial(1) == 1
-    assert factorial(3) == 6
-    assert factorial(4) == 24
-    assert factorial(6) == 720
+    @tail_recursive(nested_call_mode=NestedCallMode.DO_NOT_RESOLVE_NESTED_CALLS)
+    def factorial(n):
+        if n == 1:
+            return n
+        return mul.tail_call(n, factorial.tail_call(n - 1))
+
+    with pytest.raises(Exception):
+        assert factorial(3) == non_recursive_factorial(3) == 6
+
+
+def test_multi_function_factorial_succeeds_with_nested_tail_call_resolution_enabled():
+
+    @tail_recursive
+    def mul(a, b):
+        return a * b
+
+    @tail_recursive(nested_call_mode=NestedCallMode.RESOLVE_NESTED_CALLS)
+    def factorial(n):
+        if n == 1:
+            return n
+        return mul.tail_call(n, factorial.tail_call(n - 1))
+
+    assert factorial(1) == non_recursive_factorial(1) == 1
+    assert factorial(3) == non_recursive_factorial(3) == 6
+    assert factorial(4) == non_recursive_factorial(4) == 24
+    assert factorial(6) == non_recursive_factorial(6) == 720
 
     n = sys.getrecursionlimit() + 100
-    expected_result = 1
-    for i in range(2, n + 1):
-        expected_result *= i
-    assert factorial(n) == expected_result
+    assert factorial(n) == non_recursive_factorial(n)
+
+
+def test_factorial_succeeds_with_lru_cache_and_tail_recursion():
+    import functools
+
+    for nested_call_mode in (NestedCallMode.RESOLVE_NESTED_CALLS, NestedCallMode.DO_NOT_RESOLVE_NESTED_CALLS):
+        @tail_recursive(nested_call_mode=nested_call_mode)
+        @functools.lru_cache
+        def factorial(n, accumulator=1):
+            if n == 1:
+                return accumulator
+            return factorial.tail_call(n - 1, n * accumulator)
+
+        assert factorial(1) == non_recursive_factorial(1) == 1
+        assert factorial(3) == non_recursive_factorial(3) == 6
+        assert factorial(4) == non_recursive_factorial(4) == 24
+        assert factorial(6) == non_recursive_factorial(6) == 720
+
+        n = sys.getrecursionlimit() + 100
+        assert factorial(n) == non_recursive_factorial(n)
+
+
+def non_recursive_fibonacci(n):
+    current_fib = 0
+    next_fib = 1
+    for _ in range(n):
+        last_fib = current_fib
+        current_fib = next_fib
+        next_fib = last_fib + current_fib
+    return current_fib
 
 
 def test_fibonacci_fails_when_max_recursion_depth_is_reached():
+    for nested_call_mode in (NestedCallMode.RESOLVE_NESTED_CALLS, NestedCallMode.DO_NOT_RESOLVE_NESTED_CALLS):
 
-    @tail_recursive
-    def fibonacci(n, a=0, b=1):
-        if n == 0:
-            return a
-        elif n == 1:
-            return b
-        return fibonacci(n - 1, b, a + b)
+        @tail_recursive(nested_call_mode=nested_call_mode)
+        def fibonacci(n, a=0, b=1):
+            if n == 0:
+                return a
+            elif n == 1:
+                return b
+            return fibonacci(n - 1, b, a + b)
 
-    assert fibonacci(0) == 0
-    assert fibonacci(1) == 1
-    assert fibonacci(4) == 3
-    assert fibonacci(7) == 13
+        assert fibonacci(0) == non_recursive_fibonacci(0) == 0
+        assert fibonacci(1) == non_recursive_fibonacci(1) == 1
+        assert fibonacci(4) == non_recursive_fibonacci(4) == 3
+        assert fibonacci(7) == non_recursive_fibonacci(7) == 13
 
-    n = sys.getrecursionlimit() + 1
-    with pytest.raises(RecursionError) as excinfo:
-        fibonacci(n)
-    assert "maximum recursion depth" in str(excinfo.value)
+        n = sys.getrecursionlimit() + 1
+        with pytest.raises(RecursionError) as excinfo:
+            fibonacci(n)
+        assert "maximum recursion depth" in str(excinfo.value)
 
 
 def test_fibonacci_succeeds_with_tail_recursion():
+    for nested_call_mode in (NestedCallMode.RESOLVE_NESTED_CALLS, NestedCallMode.DO_NOT_RESOLVE_NESTED_CALLS):
+
+        @tail_recursive(nested_call_mode=nested_call_mode)
+        def fibonacci(n, a=0, b=1):
+            if n == 0:
+                return a
+            elif n == 1:
+                return b
+            return fibonacci.tail_call(n - 1, b, a + b)
+
+        assert fibonacci(0) == non_recursive_fibonacci(0) == 0
+        assert fibonacci(1) == non_recursive_fibonacci(1) == 1
+        assert fibonacci(4) == non_recursive_fibonacci(4) == 3
+        assert fibonacci(7) == non_recursive_fibonacci(7) == 13
+
+        n = sys.getrecursionlimit() + 100
+        assert fibonacci(n) == non_recursive_fibonacci(n)
+
+
+def test_multi_function_fibonacci_fails_with_nested_tail_call_resolution_disabled():
+    import functools
 
     @tail_recursive
-    def fibonacci(n, a=0, b=1):
-        if n == 0:
-            return a
-        elif n == 1:
-            return b
-        return fibonacci.tail_call(n - 1, b, a + b)
+    def add(a, b):
+        return a + b
 
-    assert fibonacci(0) == 0
-    assert fibonacci(1) == 1
-    assert fibonacci(4) == 3
-    assert fibonacci(7) == 13
+    @tail_recursive(nested_call_mode="do_not_resolve_nested_calls")
+    @functools.lru_cache
+    def fibonacci(n):
+        if n <= 1:
+            return n
+        return add.tail_call(fibonacci.tail_call(n - 1), fibonacci.tail_call(n - 2))
 
-    n = sys.getrecursionlimit() + 100
-    last = 0
-    expected_result = 1
-    for _ in range(n - 1):
-        new_last = expected_result
-        expected_result = last + expected_result
-        last = new_last
-    assert fibonacci(n) == expected_result
+    with pytest.raises(Exception):
+        fibonacci(4)
+
+
+def test_multi_function_fibonacci_succeeds_with_nested_tail_call_resolution_enabled():
+    import functools
+
+    @tail_recursive
+    def add(a, b):
+        return a + b
+
+    @tail_recursive(nested_call_mode="resolve_nested_calls")
+    # Requires ``lru_cache`` because this version of fibonacci is highly inefficient.
+    @functools.lru_cache
+    def fibonacci(n):
+        if n <= 1:
+            return n
+        return add.tail_call(fibonacci.tail_call(n - 1), fibonacci.tail_call(n - 2))
+
+    assert fibonacci(0) == non_recursive_fibonacci(0) == 0
+    assert fibonacci(1) == non_recursive_fibonacci(1) == 1
+    assert fibonacci(4) == non_recursive_fibonacci(4) == 3
+    assert fibonacci(7) == non_recursive_fibonacci(7) == 13
+
+    n = sys.getrecursionlimit() + 1
+    assert fibonacci(n) == non_recursive_fibonacci(n)
